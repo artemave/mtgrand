1f0231972f967ef1d8c749e50f9d2ad476b31ebf
artem <artem@colinux.(none)>
2010-01-07 23:43:39 +0000

    now calculating probability for deck to contain x consequent lands

--- test.rb
+++ test.rb
@@ -4,9 +4,9 @@ require 'rubygems'
 require 'facets'
 
 MANY = 10000
-res = Hash.new {|h,k| h[k] = 0}
+res = Hash.new {|h,k| h[k] = {}}
 
-MANY.times do
+MANY.times do |t|
   land_set = 0
 
   DraftDeck.new.each do |card|
@@ -17,13 +17,15 @@ MANY.times do
     end
 
     1.upto(land_set) do |l|
-      res[l] += 1
+      res[t][l] = true
     end
   end
 end
 
-# formatting result to %age
-total_num_of_sets = res.values.sum
-res.update_values{ |v| "%.4f%%" % ((v.to_f / total_num_of_sets).round_at(6)*100) }
+final = Hash.new {|h,k| h[k] = 0}
+res.each_value {|v| v.each_key {|k| final[k] += 1 }}
 
-pp res
+# formatting result to %age
+final.update_values{ |v| "%.4f%%" % ((v.to_f / MANY).round_at(6)*100) }
+
+pp final
